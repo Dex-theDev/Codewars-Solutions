73ea32b3537f6274143c39d1961817ba8daaeb13
Jaime <jaime.joseph1991@gmail.com>
2022-06-24 21:16:40 -0400

    solution

---
+++ codewars113.js
@@ -0,0 +1,8 @@
+/*
+Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+
+*/
+
+
+
+const isDivideBy = (number, a,b) => !(number % a) && !(number % b)
